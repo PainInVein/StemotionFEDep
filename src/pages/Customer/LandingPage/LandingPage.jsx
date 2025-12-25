e896a7e5df90a097d0d1125ddcc5c396fb40ba98
NguyenNgocMinh-SE182009 <nguyenngocminh17012004@gmail.com>
2025-12-25 21:13:13 +0700

    Create lesson component and update UI landing page

--- src/pages/Customer/LandingPage/LandingPage.jsx
+++ src/pages/Customer/LandingPage/LandingPage.jsx
@@ -8,7 +8,7 @@ import Backround_02 from '../../../assets/bg-02.png'
 import LoginModal from "../LoginPage/LoginPage";
 import { FaCheck } from "react-icons/fa";
 import { HiOutlineBolt, HiBolt } from "react-icons/hi2";
-import './LandingPage.css';
+import Lesson from '../../../components/common/Lesson.jsx';
 
 const LandingPage = () => {
   const features = [
@@ -31,6 +31,12 @@ const LandingPage = () => {
       gradient: "from-orange-500 to-yellow-500",
     },
   ];
+
+  const progress = {
+    lesson1: "done",
+    lesson2: "doing",
+    lesson3: "not_started"
+  };
 
   const [modalConfig, setModalConfig] = useState({ isOpen: false, role: 'student' });
 
@@ -160,106 +166,14 @@ const LandingPage = () => {
               </div>
             </div>
             <div className="flex-1 p-10">
-
-              <div className="w-[200px] h-[110px] bg-[#879EFF] rounded-[50%] pb-5 flex justify-center">
-                <div className="w-[200px] h-[110px]
-                                border-l-[50px] border-l-transparent
-                                border-r-[50px] border-r-transparent
-                                border-b-[90px] border-b-[#A0B2FF]
-                                rounded-[100%] absolute flex justify-center">
-                  <div className="w-[160px] h-[85px] bg-[#94AEFF] rounded-[50%] pb-3 pt-2 flex justify-center absolute">
-                    <div className="w-[130px] h-[60px] bg-[#DDE4FF] rounded-[50%] flex justify-center">
-                      <div className="w-[13px] h-[60px] bg-[#94AEFF] flex justify-center relative py-2">
-                        <div className="w-[110px] h-[43px] bg-[#94AEFF] rounded-[50%] flex justify-center absolute">
-                          <div className="flex justify-center items-center h-full">
-                            <FaCheck className="text-[#DDE4FF] size-6" />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+              <div className="">
+                <Lesson status={progress.lesson1} to="#" />
               </div>
-
-              <div className="w-[240px] h-[145px] bg-[#A0B2FF] rounded-[50%] flex justify-center pt-2 pb-4 mt-10 ml-60">
-                <div className="w-[220px] h-[125px] bg-[#fff] rounded-[50%] flex justify-center pt-1">
-                  <div className="w-[200px] h-[110px] bg-[#879EFF] rounded-[50%] pb-5 flex justify-center ">
-                    <div className="w-[200px] h-[110px]
-                                border-l-[50px] border-l-transparent
-                                border-r-[50px] border-r-transparent
-                                border-b-[90px] border-b-[#A0B2FF]
-                                rounded-[100%] absolute flex justify-center">
-                      <div className="w-[160px] h-[85px] bg-[#94AEFF] rounded-[50%] pb-3 pt-2 flex justify-center absolute">
-                        <div className="w-[130px] h-[60px] bg-[#FFF] rounded-[50%] flex justify-center relative ">
-                          {/* Hinh ngoi sao */}
-                          <div className="w-[170px] h-[170px] animate-[float_2.5s_ease-in-out_infinite] absolute -top-[115px]">
-
-                            {/* thanh ngang */}
-                            <div className="absolute top-1/2 -translate-y-1/2 h-[60px] w-full bg-[#FFD28A] rounded-[45%] shadow-[inset_4px_0_8px_rgba(255,165,128,0.6),inset_-4px_0_8px_rgba(255,165,128,0.6)]" />
-                            {/* thanh dọc */}
-                            <div className="absolute left-1/2 -translate-x-1/2 w-[60px] h-full bg-[#FFD28A] rounded-[45%] shadow-[inset_0_4px_8px_rgba(255,165,128,0.6),inset_0_-4px_8px_rgba(255,165,128,0.6)]">
-                              {/* lớp che giữa */}
-                              <div className="absolute left-1/2 top-1/2 
-                -translate-x-1/2 -translate-y-1/2
-                w-[80px] h-[80px] 
-                bg-[#FFE7B0] rounded-full shadow-[0_0_35px_18px_rgba(255,240,194,0.9)]" />
-                              {/* mắt */}
-                              <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 
-                                              flex items-center">
-                                <div className="w-1 h-1 bg-black"></div>
-                                <div className="w-[30px] h-[30px] bg-none rounded-full flex justify-center items-center border-2 border-black">
-                                  <div className="w-[25px] h-[25px] bg-none rounded-full">
-                                    <div className="w-[20px] h-[20px] bg-black rounded-full mt-1 ml-1 border-2 border-white">
-
-                                    </div>
-                                  </div>
-                                </div>
-                                <div className="w-2 h-1 bg-black"></div>
-
-                                <div className="w-[30px] h-[30px] bg-none rounded-full flex justify-center items-center border-2 border-black">
-                                  <div className="w-[25px] h-[25px] bg-none rounded-full">
-                                    <div className="w-[20px] h-[20px] bg-black rounded-full mt-1 mr-1 border-2 border-white">
-
-                                    </div>
-                                  </div>
-                                </div>
-                                <div className="w-1 h-1 bg-black"></div>
-                              </div>
-
-                              {/* miệng */}
-                              <div className="absolute top-[45%] left-1/2 -translate-x-1/2">
-                                <div className="w-[50px] h-[20px] bg-black rounded-full mx-auto  shadow-[0_0_30px_12px_rgba(255,240,194,0.7)]">
-                                  <div className="w-[40px] h-[10px] bg-white rounded-full mx-auto"></div>
-                                  <div className="w-[45px] h-[6px] bg-red-400 rounded-b-full mt-[3.5px] mx-auto"></div>
-                                </div>
-                              </div>
-
-                            </div>
-
-
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+              <div className="ml-60">
+                <Lesson status={progress.lesson2} to="#" />
               </div>
-
-              <div className="w-[200px] h-[110px] bg-[#999999] rounded-[50%] pb-5 flex justify-center mt-10 relative">
-                <div className="w-[200px] h-[110px]
-                                border-l-[50px] border-l-transparent
-                                border-r-[50px] border-r-transparent
-                                border-b-[90px] border-b-[#ADADAD]
-                                rounded-[100%] absolute flex justify-center">
-                  <div className="w-[160px] h-[85px] bg-[#E5E5E5] rounded-[50%] pb-3 pt-2 flex justify-center absolute">
-                    <div className="w-[130px] h-[60px] bg-[#CCCCCC] rounded-[50%] flex justify-center">
-                      <div className="w-[13px] h-[60px] bg-[#E5E5E5] flex justify-center relative py-2">
-                        <div className="w-[110px] h-[43px] bg-[#E5E5E5] rounded-[50%] flex justify-center absolute">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+              <div className="">
+                <Lesson status={progress.lesson3} to="#" />
               </div>
             </div>
           </div>
@@ -332,10 +246,10 @@ const LandingPage = () => {
             ))}
           </div>
         </div>
-      </section>
+      </section >
 
       {/* CTA Section */}
-      <section className="py-20 px-6 bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-500 text-white">
+      <section section className="py-20 px-6 bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-500 text-white" >
         <div className="max-w-4xl mx-auto text-center space-y-6">
           <h2 className="text-3xl md:text-5xl font-bold">
             Bắt đầu hành trình STEM của bạn ngay hôm nay
@@ -361,7 +275,7 @@ const LandingPage = () => {
           </div>
         </div>
       </section>
-    </div>
+    </div >
   );
 };
 
