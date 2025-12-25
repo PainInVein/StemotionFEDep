7b2993b97dfcfc2eccfc088082e24f6c8c50135d
NguyenNgocMinh-SE182009 <nguyenngocminh17012004@gmail.com>
2025-12-25 17:38:09 +0700

    #2 update UI landing page

--- src/pages/Customer/LandingPage/LandingPage.jsx
+++ src/pages/Customer/LandingPage/LandingPage.jsx
@@ -8,6 +8,7 @@ import Backround_02 from '../../../assets/bg-02.png'
 import LoginModal from "../LoginPage/LoginPage";
 import { FaCheck } from "react-icons/fa";
 import { HiOutlineBolt, HiBolt } from "react-icons/hi2";
+import './LandingPage.css';
 
 const LandingPage = () => {
   const features = [
@@ -189,7 +190,54 @@ const LandingPage = () => {
                                 border-b-[90px] border-b-[#A0B2FF]
                                 rounded-[100%] absolute flex justify-center">
                       <div className="w-[160px] h-[85px] bg-[#94AEFF] rounded-[50%] pb-3 pt-2 flex justify-center absolute">
-                        <div className="w-[130px] h-[60px] bg-[#FFF] rounded-[50%] flex justify-center">
+                        <div className="w-[130px] h-[60px] bg-[#FFF] rounded-[50%] flex justify-center relative ">
+                          {/* Hinh ngoi sao */}
+                          <div className="w-[170px] h-[170px] animate-[float_2.5s_ease-in-out_infinite] absolute -top-[115px]">
+
+                            {/* thanh ngang */}
+                            <div className="absolute top-1/2 -translate-y-1/2 h-[60px] w-full bg-[#FFD28A] rounded-[45%] shadow-[inset_4px_0_8px_rgba(255,165,128,0.6),inset_-4px_0_8px_rgba(255,165,128,0.6)]" />
+                            {/* thanh dọc */}
+                            <div className="absolute left-1/2 -translate-x-1/2 w-[60px] h-full bg-[#FFD28A] rounded-[45%] shadow-[inset_0_4px_8px_rgba(255,165,128,0.6),inset_0_-4px_8px_rgba(255,165,128,0.6)]">
+                              {/* lớp che giữa */}
+                              <div className="absolute left-1/2 top-1/2 
+                -translate-x-1/2 -translate-y-1/2
+                w-[80px] h-[80px] 
+                bg-[#FFE7B0] rounded-full shadow-[0_0_35px_18px_rgba(255,240,194,0.9)]" />
+                              {/* mắt */}
+                              <div className="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                                              flex items-center">
+                                <div className="w-1 h-1 bg-black"></div>
+                                <div className="w-[30px] h-[30px] bg-none rounded-full flex justify-center items-center border-2 border-black">
+                                  <div className="w-[25px] h-[25px] bg-none rounded-full">
+                                    <div className="w-[20px] h-[20px] bg-black rounded-full mt-1 ml-1 border-2 border-white">
+
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="w-2 h-1 bg-black"></div>
+
+                                <div className="w-[30px] h-[30px] bg-none rounded-full flex justify-center items-center border-2 border-black">
+                                  <div className="w-[25px] h-[25px] bg-none rounded-full">
+                                    <div className="w-[20px] h-[20px] bg-black rounded-full mt-1 mr-1 border-2 border-white">
+
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="w-1 h-1 bg-black"></div>
+                              </div>
+
+                              {/* miệng */}
+                              <div className="absolute top-[45%] left-1/2 -translate-x-1/2">
+                                <div className="w-[50px] h-[20px] bg-black rounded-full mx-auto  shadow-[0_0_30px_12px_rgba(255,240,194,0.7)]">
+                                  <div className="w-[40px] h-[10px] bg-white rounded-full mx-auto"></div>
+                                  <div className="w-[45px] h-[6px] bg-red-400 rounded-b-full mt-[3.5px] mx-auto"></div>
+                                </div>
+                              </div>
+
+                            </div>
+
+
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -213,10 +261,7 @@ const LandingPage = () => {
                   </div>
                 </div>
               </div>
-
-
             </div>
-
           </div>
 
           <div className="flex flex-row bg-gray-100">
