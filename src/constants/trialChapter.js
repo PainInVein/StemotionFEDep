0fb8ed33e515eb9168965b6a0f6ddf85cea321a8
PainInVein <lesybinh2004@gmail.com>
2026-02-25 13:05:05 +0700

    fix trial id

--- src/constants/trialChapter.js
+++ src/constants/trialChapter.js
@@ -1,3 +1,3 @@
 // src/constants/trialChapter.js
 // ✅ Điền chapterId thật từ BE vào đây
-export const TRIAL_CHAPTER_ID = "fa06337c-f205-4963-b0c8-02da546d727e";
+export const TRIAL_CHAPTER_ID = "ac9511fa-4b87-48d9-a7a1-a9aa95ab94b9";
